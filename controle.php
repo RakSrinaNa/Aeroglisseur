567285e395223e839a1e83a2880058ec55ae4996
MrCraftCod <zerderr@gmail.com>
2013-12-14 18:40:55 +0100

    Adding web page

---
+++ controle.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Controle de l'arduino</title>
+    </head>
+    <body>
+        <form method="get" action="index.php">
+            <p>
+            	Vitesse:
+				<input type="text" name="vi" />
+            </p>
+            <p>
+            	Orientation:
+                <input type="text" name="or" />
+            </p>
+	    <p>
+            	Sustentation:
+                <input type="text" name="st" />
+            </p>
+            <p>
+				<input type="submit" value="Valider" />
+            </p>
+        </form>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<html>
+	<head>
+		<title>Resultats de la requete</title>
+	</head>
+	<body>
+		<?php
+			if(isset($_GET['vi']))
+			{
+		                $lines = file('test.txt');
+               			$lines[0] = 'vi='.$_GET['vi']."\n";
+         			$new_content = implode('', $lines);
+              			$h = fopen('test.txt', 'w');
+                		fwrite($h, $new_content);
+                		fclose($h);
+				echo '<br>';
+				echo 'Vitesse modifiee a: ';
+				echo $_GET['vi'];
+			}
+			if(isset($_GET['or']))
+			{
+                		$lines = file('test.txt');
+                		$lines[1] = 'or='.$_GET['or']."\n";
+                		$new_content = implode('', $lines);
+                		$h = fopen('test.txt', 'w');
+                		fwrite($h, $new_content);
+                		fclose($h);
+				echo '<br>';
+				echo 'Orientation modifiee a: ';
+				echo $_GET['or'];
+			}
+			if(isset($_GET['st']))
+			{
+                		$lines = file('test.txt');
+                		$lines[2] = 'st='.$_GET['st']."\n";
+                		$new_content = implode('', $lines);
+                		$h = fopen('test.txt', 'w');
+                		fwrite($h, $new_content);
+                		fclose($h);
+				echo '<br>';
+				echo 'Sustentation modifiee a: ';
+				echo $_GET['st'];
+			}
+		?>
+	</body>
+</html>
