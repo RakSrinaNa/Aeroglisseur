9fa93002e26abd9164aaedc6305329303cda8f46
MrCraftCod <zerderr@gmail.com>
2013-12-14 23:43:05 +0100

    Fix indent and add view of current values

--- index.php
+++ index.php
@@ -6,40 +6,45 @@
 		<?php
 			if(isset($_GET['vi']))
 			{
-		                $lines = file('test.txt');
-               			$lines[0] = 'vi='.$_GET['vi']."\n";
-         			$new_content = implode('', $lines);
-              			$h = fopen('test.txt', 'w');
-                		fwrite($h, $new_content);
-                		fclose($h);
+		        $lines = file('variables.txt');
+               	$lines[0] = 'vi='.$_GET['vi']."\n";
+         		$new_content = implode('', $lines);
+              	$h = fopen('variables.txt', 'w');
+                fwrite($h, $new_content);
+                fclose($h);
 				echo '<br>';
 				echo 'Vitesse modifiee a: ';
 				echo $_GET['vi'];
 			}
 			if(isset($_GET['or']))
 			{
-                		$lines = file('test.txt');
-                		$lines[1] = 'or='.$_GET['or']."\n";
-                		$new_content = implode('', $lines);
-                		$h = fopen('test.txt', 'w');
-                		fwrite($h, $new_content);
-                		fclose($h);
+                $lines = file('variables.txt');
+                $lines[1] = 'or='.$_GET['or']."\n";
+                $new_content = implode('', $lines);
+                $h = fopen('variables.txt', 'w');
+                fwrite($h, $new_content);
+                fclose($h);
 				echo '<br>';
 				echo 'Orientation modifiee a: ';
 				echo $_GET['or'];
 			}
 			if(isset($_GET['st']))
 			{
-                		$lines = file('test.txt');
-                		$lines[2] = 'st='.$_GET['st']."\n";
-                		$new_content = implode('', $lines);
-                		$h = fopen('test.txt', 'w');
-                		fwrite($h, $new_content);
-                		fclose($h);
+                $lines = file('variables.txt');
+                $lines[2] = 'st='.$_GET['st']."\n";
+                $new_content = implode('', $lines);
+                $h = fopen('variables.txt', 'w');
+                fwrite($h, $new_content);
+                fclose($h);
 				echo '<br>';
 				echo 'Sustentation modifiee a: ';
 				echo $_GET['st'];
 			}
+            echo '<br><br>Current variables.txt file:<br><br>';
+	foreach(file('variables.txt') as $temp)
+	{
+            echo $temp.'<br>';
+	}
 		?>
 	</body>
 </html>
